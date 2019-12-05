b66a0fc1c678b2ea66f821f5dff66667dad32c9a
LAC1213 <lambertacampo@gmail.com>
2019-12-05 18:50:22 +0000

    comments + magical speedup

--- src/compact_unit_ball.lean
+++ src/compact_unit_ball.lean
@@ -15,6 +15,14 @@ import analysis.normed_space.finite_dimension
 noncomputable theory
 open_locale classical
 
+local attribute [instance, priority 10000] mul_action.to_has_scalar distrib_mul_action.to_mul_action
+semimodule.to_distrib_mul_action module.to_semimodule vector_space.to_module normed_space.to_vector_space
+ring.to_monoid normed_ring.to_ring normed_field.to_normed_ring add_group.to_add_monoid
+add_comm_group.to_add_group normed_group.to_add_comm_group ring.to_semiring add_comm_group.to_add_comm_monoid
+normed_field.to_discrete_field
+
+set_option class.instance_max_depth 36
+
 open metric set
 
 variables
@@ -22,68 +30,51 @@ variables
 
 lemma two_positive : (2 : ℝ) > 0 :=
 begin
-  linarith,
+  norm_num,
 end
 
 lemma half_positive : (1/2 : ℝ) > 0 :=
 begin
-  simp,
-  linarith,
+  norm_num,
 end
 
-lemma ball_span_ash {A : set V} : 
-(∀ v : V, norm v ≤ 1 → v ∈ submodule.span ℝ A) 
-→ ∀ v : V, v ∈ submodule.span ℝ A :=
-
+-- I am moving hypotheses before the colon so you don't have to write "intro" at the
+-- beginning of the proof
+lemma ball_span_ash {A : set V} (hyp : ∀ v : V, norm v ≤ 1 → v ∈ submodule.span ℝ A) (v : V) :
+  v ∈ submodule.span ℝ A :=
 begin
+  cases (classical.em (v = 0)) with v0 vn0,
+  { rw submodule.mem_span,
+    intros p hp,
+    rw v0,
+    exact submodule.zero_mem p,
+  },
+  set w : V := (1/norm v) • v with hwdef, -- "set" = "let + have".
+  have hw2 : norm v ≠ 0 := mt (norm_eq_zero v).1 vn0,
+  have hw : norm w ≤ 1,
+    have hw1 : norm w = 1,
+      rw hwdef,
+      rw norm_smul,
+      rw real.norm_eq_abs,
+      rw abs_of_nonneg,
+        rw mul_comm,
+        rw mul_one_div_cancel,
+        exact hw2,
+      simp,
+    rw le_iff_eq_or_lt,
+    left,
+    exact hw1,
+  have h3 : (1/norm v : ℝ) ≠ 0,
+    rw div_eq_inv_mul,
+    rw mul_one,
+    exact inv_ne_zero hw2,
 
-intro hyp,
-intro v,
-
-cases (classical.em (v = 0)) with v0 vn0,
-rw submodule.mem_span,
-intros p hp,
-rw v0,
-exact submodule.zero_mem p,
-
-let w : V := (1/norm v) • v,
-have hwdef : w = (1/norm v) • v,
-refl,
-
-have hw2 : norm v ≠ 0,
-intro hv,
-rw norm_eq_zero at hv,
-exact vn0 hv,
-
-have hw : norm w ≤ 1, 
-
-have hw1 : norm w = 1,
-rw hwdef,
-rw norm_smul,
-rw real.norm_eq_abs,
-rw abs_of_nonneg,
-rw mul_comm,
-rw mul_one_div_cancel,
-
-have h2 := hyp w,
-
-exact hw2,
-simp,
-rw le_iff_eq_or_lt,
-left,
-exact hw1,
-
-have h3 : (1/norm v : ℝ) ≠ 0,
-rw div_eq_inv_mul,
-rw mul_one,
-exact inv_ne_zero hw2,
-
-rw <- submodule.smul_mem_iff (submodule.span ℝ A) h3,
-rw <- hwdef,
-exact hyp w hw,
+  rw <- submodule.smul_mem_iff (submodule.span ℝ A) h3,
+  rw <- hwdef,
+  exact hyp w hw,
 end
 
-
+-- if A spans the closed unit ball then it spans all of V
 lemma ball_span {A : set V} : (∀ v : V, v ∈ (closed_ball 0 1 : set V) → v ∈ submodule.span ℝ A) 
 → ∀ v : V, v ∈ submodule.span ℝ A :=
 begin
@@ -95,14 +86,15 @@ begin
   exact ball_span_ash hyp,
 end
 
-
-theorem finite_dimensional_span_of_finite {V : Type} {A : set V} [normed_group V] [normed_space ℝ V] (hA : finite A) :
+theorem finite_dimensional_span_of_finite {V : Type} {A : set V} 
+  [normed_group V] [normed_space ℝ V] (hA : finite A) :
   finite_dimensional ℝ ↥(submodule.span ℝ A) :=
 begin
   apply is_noetherian_of_fg_of_noetherian,
   exact submodule.fg_def.2 ⟨A, hA, rfl⟩,
 end
 
+-- the span of a finite set is sequentially closed
 lemma finite_span_seq_closed (A : set V) : finite A → is_seq_closed (↑(submodule.span ℝ A) : set V) :=
 begin
   intro h_fin,
@@ -128,6 +120,8 @@ begin
   exact ⟨f, λx, hf x⟩
 end
 
+-- if the closed unit ball is compact, then there is 
+-- a function which selects the center of a ball of radius 1/2 close to it
 lemma compact_choice_func (hc : compact (closed_ball 0 1 : set V)) :
   ∃ A : set V, A ⊆ (closed_ball 0 1 : set V) ∧ finite A ∧
   ∃ f : V → V, ∀ x : V, f x ∈ A ∧ (x ∈ (closed_ball 0 1 : set V) 
@@ -140,11 +134,10 @@ begin
         simp,
         use x,
         rw dist_self,
-        simp,
         split,
         simp at hx,
         exact hx,
-        linarith,
+        norm_num,
     },
     obtain ⟨A, A_sub, A_fin, HcoverA⟩ :
     ∃ A ⊆ B, finite A ∧ B ⊆ ⋃ a ∈ A, ball a (0.5 : real) :=
@@ -179,6 +172,8 @@ def partial_sum (f : ℕ → V) : ℕ → V
 def approx_seq (v : V) (f : V → V) : ℕ → V :=
   partial_sum (λ n : ℕ, (1/2 : ℝ)^n • f(aux_seq v f n))
 
+-- if a sequence w n satisfies the bound |v - w n| < 1/2^(n+1) then
+-- w n converges to v
 lemma bound_power_two_convergence (w : ℕ → V) {v : V}
   (h : ∀ n : ℕ, norm (v - w n) ≤ (0.5 : ℝ)^(n + 1)) :
   filter.tendsto w filter.at_top (nhds v) :=
@@ -211,6 +206,9 @@ begin
   linarith,  
 end
 
+-- the approximation sequence (w n below) is contained in the span of A
+-- this is true since by construction w n is a linear combination 
+-- of the elements in A
 lemma approx_in_span (A : set V) (v : V) (f : V → V) 
   (hf : ∀ x : V, f x ∈ A): 
   ∀ n : ℕ, approx_seq v f n ∈ submodule.span ℝ A :=
@@ -242,164 +240,162 @@ begin
   },
 end
 
-lemma limit_in_span {S : set V} (hseq : is_seq_closed S) 
-  {w : ℕ → V} (hw : ∀ n : ℕ, w n ∈ S) 
-  {v : V} (hlim : filter.tendsto w filter.at_top (nhds v : filter V)) :
-  v ∈ S :=
-begin
-  exact mem_of_is_seq_closed hseq hw hlim,
-end
-
 theorem compact_unit_ball_implies_finite_dim : 
-    compact (closed_ball 0 1 : set V) → vector_space.dim ℝ V < cardinal.omega :=
+    compact (closed_ball 0 1 : set V) → finite_dimensional ℝ V :=
 begin
-    intro Hcomp,
-    obtain ⟨A, A_sub, A_fin, Hexistsf⟩ := compact_choice_func Hcomp,
-    obtain ⟨f, hf⟩ := Hexistsf,
+  intro Hcomp,
+  -- us compactness to find a finite set A
+  -- and function f : V -> A such that for every 
+  -- v in closed_ball 0 1, |f v - v| < 1/2
+  obtain ⟨A, A_sub, A_fin, Hexistsf⟩ := compact_choice_func Hcomp,
+  obtain ⟨f, hf⟩ := Hexistsf,
 
-    let B : set V := closed_ball 0 1,
+  -- suffices to show closed_ball 0 1 is spanned by A
+  apply finite_dimensional.of_fg,
+  rw submodule.fg_def,
+  existsi A,
+  split,
+  exact A_fin,
+  rw submodule.eq_top_iff',
+  apply ball_span,
 
-    -- suffices to show B is spanned by A
-    rw <- finite_dimensional.finite_dimensional_iff_dim_lt_omega,
-    apply finite_dimensional.of_fg,
+  -- let v in closed_ball 0 1 and show that it is in the span of A
+  -- to do so we construct a sequence w n such that
+  -- w n -> v and w n in span A for all n 
+  -- for this we do a kind of 'dyadic approximation' of v using the set A
+  -- then we use that the span of a finite set is closed to conclude
+  intros v hv,
+  let u : ℕ → V := aux_seq v f,
+  let w : ℕ → V := partial_sum (λ x : ℕ, (0.5 : ℝ)^x • f(u(x))),
 
-    rw submodule.fg_def,
-    existsi A,
-    split,
-    exact A_fin,
-    rw submodule.eq_top_iff',
-    apply ball_span,
+  -- show that w n is in the span of A
+  have hw : ∀ n, w n ∈ submodule.span ℝ A,
+  {
+    have hf' : ∀ x : V, f x ∈ A,
+    intro x, 
+    exact (hf x).1,
+    exact approx_in_span A v f hf',
+  },
 
-    intros v hv,
-    let u : ℕ → V := aux_seq v f,
-    let w : ℕ → V := partial_sum (λ x : ℕ, (0.5 : ℝ)^x • f(u(x))),
-
-    -- want to show that w n in span A and  w n -> v as n -> infty
-
-    have hw : ∀ n, w n ∈ submodule.span ℝ A,
+  -- this is just some algebraic manipulation
+  have hdist : ∀ n : ℕ, v - w n = (0.5 : real)^(n+1) • u (n+1),
+  {
+    intro n,
+    induction n with n hn,
     {
-      have hf' : ∀ x : V, f x ∈ A,
-      intro x, 
-      exact (hf x).1,
-      exact approx_in_span A v f hf',
-    },
-
-    -- this is just some algebraic manipulation
-    have hdist : ∀ n : ℕ, v - w n = (0.5 : real)^(n+1) • u (n+1),
-    {
-      intro n,
-      induction n with n hn,
-      {
-        have h1 : w 0 = (0.5 : real)^0 • f(v), refl,
-        rw zero_add,
-        rw pow_one,
-        rw pow_zero at h1,
-        rw one_smul at h1,
-        rw h1,
-        have h2 : u 1 = (2: ℝ) • (v - f(v)), refl,
-        rw h2,
-        rw smul_smul,
-        rw one_div_mul_cancel,
-        rw one_smul,
-        exact two_ne_zero',
-      }, {
-        have h1 : w (n+1) = (0.5 : real)^(n+1) • f(u (n+1)) + w(n), refl,
-        have h2 : u (n+2) = (2 : ℝ) • (u (n+1) - f (u (n+1))), refl,
-        rw h2,
-        rw pow_succ,
-        rw smul_smul,
-        rw mul_comm,
-        rw <- mul_assoc,
-        rw mul_one_div_cancel,
-        rw one_mul,
-        rw smul_sub,
-        rw <- hn,
-        rw h1,
-        rw add_comm _ (w n),
-        rw sub_sub v (w n) _,
-        exact two_ne_zero',
-      },
-    },
-
-    -- main bound for convergence using the expression hdist
-    have hbound : ∀ n : ℕ, norm (v - w n) ≤ (0.5 : ℝ)^(n+1),
-    {
-      intro n,
-      rw hdist n,
-      rw norm_smul,
-      rw real.norm_eq_abs,
-      rw abs_of_nonneg,
-      have hu : u(n+1) = (2 : ℝ) • (u(n) - f(u(n))), refl,
-      rw hu,
-      have husmall : norm (u n) ≤ 1,
-      {
-        induction n with n hn,
-        have h0 : u 0 = v, refl,
-        rw h0,
-        rw <- dist_zero_right,
-        exact hv,
-
-        have hu' : u(n + 1) = (2 : ℝ) • (u(n) - f(u(n))), refl,
-        rw hu',
-        have h2 := (hf (u n)).2,
-        rw norm_smul,
-        rw real.norm_eq_abs,
-        rw <- dist_eq_norm,
-        rw abs_of_nonneg,
-        have h3 := hn hu',
-        rw <- dist_zero_right at h3,
-        have h4 : dist (u n) (f (u n)) < 1/2,
-        exact h2 h3,
-        rw le_iff_eq_or_lt,
-        right,
-        rw <- mul_lt_mul_left two_positive at h4,  
-        rw mul_one_div_cancel at h4,
-        exact h4,   
-        exact two_ne_zero',
-        linarith,
-      },
-      have h2 : 0 < (1/2 : ℝ) := half_positive,
-      have h1 : 0 < (1/2 : ℝ)^(n+1),
-      exact pow_pos h2 (n+1),
-      have h3 : (1/2 : ℝ)^(n+1) * norm ((2:ℝ) • (u n - f(u n))) ≤ (1/2 : ℝ)^(n+1) * 1 → 
-        (1/2 : ℝ)^(n+1) * norm ((2:ℝ) • (u n - f(u n))) ≤ (1/2 : ℝ)^(n+1),
-      intro h,
-      rw mul_one at h,
-      exact h,
-      apply h3,
-      rw mul_le_mul_left h1,
-      rw norm_smul,
-      rw <- dist_zero_right at husmall,
-      have h4 := (hf (u n)).2 husmall,
-      rw <- dist_eq_norm,
-      rw real.norm_eq_abs,
-      rw abs_of_nonneg,
-      rw <- mul_le_mul_left h2,
-      rw <- mul_assoc,
-      rw mul_one,
+      have h1 : w 0 = (0.5 : real)^0 • f(v), refl,
+      rw zero_add,
+      rw pow_one,
+      rw pow_zero at h1,
+      rw one_smul at h1,
+      rw h1,
+      have h2 : u 1 = (2: ℝ) • (v - f(v)), refl,
+      rw h2,
+      rw smul_smul,
       rw one_div_mul_cancel,
+      rw one_smul,
+      exact two_ne_zero',
+    }, {
+      have h1 : w (n+1) = (0.5 : real)^(n+1) • f(u (n+1)) + w(n), refl,
+      have h2 : u (n+2) = (2 : ℝ) • (u (n+1) - f (u (n+1))), refl,
+      rw h2,
+      rw pow_succ,
+      rw smul_smul,
+      rw mul_comm,
+      rw <- mul_assoc,
+      rw mul_one_div_cancel,
       rw one_mul,
+      rw smul_sub,
+      rw <- hn,
+      rw h1,
+      rw add_comm _ (w n),
+      rw sub_sub v (w n) _,
+      exact two_ne_zero',
+    },
+  },
+
+  -- main bound for convergence using the expression hdist
+  have hbound : ∀ n : ℕ, norm (v - w n) ≤ (0.5 : ℝ)^(n+1),
+  {
+    intro n,
+    rw hdist n,
+    rw norm_smul,
+    rw real.norm_eq_abs,
+    rw abs_of_nonneg,
+    have hu : u(n+1) = (2 : ℝ) • (u(n) - f(u(n))), refl,
+    rw hu,
+    have husmall : norm (u n) ≤ 1,
+    {
+      induction n with n hn,
+      have h0 : u 0 = v, refl,
+      rw h0,
+      rw <- dist_zero_right,
+      exact hv,
+
+      have hu' : u(n + 1) = (2 : ℝ) • (u(n) - f(u(n))), refl,
+      rw hu',
+      have h2 := (hf (u n)).2,
+      rw norm_smul,
+      rw real.norm_eq_abs,
+      rw <- dist_eq_norm,
+      rw abs_of_nonneg,
+      have h3 := hn hu',
+      rw <- dist_zero_right at h3,
+      have h4 : dist (u n) (f (u n)) < 1/2,
+      exact h2 h3,
       rw le_iff_eq_or_lt,
       right,
-      exact h4,
+      rw <- mul_lt_mul_left two_positive at h4,  
+      rw mul_one_div_cancel at h4,
+      exact h4,   
       exact two_ne_zero',
       linarith,
-      rw ge_iff_le,
-      rw le_iff_eq_or_lt,
-      right,
-      exact pow_pos half_positive (n+1),
     },
+    have h2 : 0 < (1/2 : ℝ) := half_positive,
+    have h1 : 0 < (1/2 : ℝ)^(n+1),
+    exact pow_pos h2 (n+1),
+    have h3 : (1/2 : ℝ)^(n+1) * norm ((2:ℝ) • (u n - f(u n))) ≤ (1/2 : ℝ)^(n+1) * 1 → 
+      (1/2 : ℝ)^(n+1) * norm ((2:ℝ) • (u n - f(u n))) ≤ (1/2 : ℝ)^(n+1),
+    intro h,
+    rw mul_one at h,
+    exact h,
+    apply h3,
+    rw mul_le_mul_left h1,
+    rw norm_smul,
+    rw <- dist_zero_right at husmall,
+    have h4 := (hf (u n)).2 husmall,
+    rw <- dist_eq_norm,
+    rw real.norm_eq_abs,
+    rw abs_of_nonneg,
+    rw <- mul_le_mul_left h2,
+    rw <- mul_assoc,
+    rw mul_one,
+    rw one_div_mul_cancel,
+    rw one_mul,
+    rw le_iff_eq_or_lt,
+    right,
+    exact h4,
+    exact two_ne_zero',
+    linarith,
+    rw ge_iff_le,
+    rw le_iff_eq_or_lt,
+    right,
+    exact pow_pos half_positive (n+1),
+  },
 
-    have hlim : filter.tendsto w filter.at_top (nhds v : filter V)
+  -- w n -> v as n -> infty
+  have hlim : filter.tendsto w filter.at_top (nhds v : filter V)
     := bound_power_two_convergence w hbound,
     
-    let S : set V := ↑(submodule.span ℝ A),
-    have hspan_closed : is_seq_closed S := finite_span_seq_closed A A_fin,
+  -- the span of a finite set is (sequentially) closed
+  let S : set V := ↑(submodule.span ℝ A),
+  have hspan_closed : is_seq_closed S := finite_span_seq_closed A A_fin,
 
-    have hw' : ∀ n, w n ∈ S,
-    exact hw,
+  have hw' : ∀ n, w n ∈ S,
+  exact hw,
 
-    have hinS: v ∈ S,
-    exact limit_in_span hspan_closed hw' hlim,
-    exact hinS,
+  -- since S is closed, the limit v of w n is in S, as required.
+  have hinS: v ∈ S,
+  exact mem_of_is_seq_closed hspan_closed hw' hlim,
+  exact hinS,
 end
